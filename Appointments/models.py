3a6fac3adfafcefe6d2b43b445979f61f5680566
venkata1302 <36800100+venkata1302@users.noreply.github.com>
2018-05-18 16:10:09 -0400

    Update models.py

--- Appointments/models.py
+++ Appointments/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Appointment(models.Model):
-    date_time = models.DateTimeField()
+    date_time = models.DateTimeField(unique = True)
     description = models.CharField(max_length = 20)
 
